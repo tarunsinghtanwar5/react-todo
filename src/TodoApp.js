e97e7f6313d8820be3c402429510c79a0a9629c1
tarunsinghtanwar <tarunsinghtanwar14@gmail.com>
2021-08-08 17:12:27 +0530

    refactoring

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect } from 'react';
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 import AppBar from '@material-ui/core/AppBar'
@@ -15,24 +15,7 @@ function Todo() {
         defaultTodos
     );
 
-    // const [todos, setTodos] = useState(defaultTodos);
-    // const addTodo = newTodoText => {
-    //     setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }])
-    // }
-    // const removeTodo=todoId => {
-    //     const updatedTodos =todos.filter(todo => todo.id !==todoId);
-    //     setTodos(updatedTodos);
-    // } 
-    // const toggleTodo=todoId => {
-    //     const updatedTodos =todos.map(todo =>todo.id===todoId ? {...todo,completed: !todo.completed} : todo)
-    //     setTodos(updatedTodos);
-
-    // }
-    // const editTodo = ((todoId,newTask) => {
-    //     const updatedTodos = todos.map(todo => todo.id === todoId ? { ...todo, task:newTask } : todo)
-    //     setTodos(updatedTodos);
-
-    // })
+    
     useEffect(() => {
         window.localStorage.setItem("todos", JSON.stringify(todos));
     }, [todos]);
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import Todo from "./Todo";
 
